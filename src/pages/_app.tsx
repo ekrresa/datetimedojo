fd0edd80bf5072c39cd9e28f6e0fbfb977ab36bd
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-12-26 19:01:18 +0100

    Disable seo tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import '@/styles/globals.css'
 import { GeistSans } from 'geist/font/sans'
+import { NextSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={GeistSans.variable}>
+      <NextSeo
+        title="Date-Time Dojo"
+        description="A solution for converting dates into different formats."
+        noindex
+        nofollow
+      />
       <Component {...pageProps} />
     </main>
   )
