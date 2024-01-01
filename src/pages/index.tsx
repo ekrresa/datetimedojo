f5870c01119878092b6364547363f7c8e89eb98b
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2024-01-01 16:47:32 +0100

    Fix hydration errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import * as React from 'react'
 import { Button, DateField, DateInput, DateSegment } from 'react-aria-components'
-import { CalendarDays, CopyIcon } from 'lucide-react'
+import { CalendarDays } from 'lucide-react'
 import {
   formatISO,
   formatISO9075,
@@ -9,74 +10,78 @@ import {
   getUnixTime,
   parseISO,
 } from 'date-fns'
+
 import { useCurrentTime } from '@/hooks/useCurrentTime'
-import clientOnly from 'next/dynamic'
 import { convertDateToExcelFormat } from '@/helpers'
-import { ClientOnly } from '@/components/ClientOnly'
 import { DateDisplay } from '@/components/DateDisplay'
+import { useIsMounted } from '@/hooks/useIsMounted'
 
 export default function Home() {
-  const currentTime = useCurrentTime()
+  const { currentTime } = useCurrentTime()
+  const isMounted = useIsMounted()
 
-  const time = getTime(currentTime ?? new Date())
-  const unixTime = getUnixTime(currentTime ?? new Date())
-  const ISODate = parseISO(new Date(time).toISOString())
+  const time = getTime(currentTime)
+  const unixTime = getUnixTime(currentTime)
+  const ISODate = parseISO(currentTime.toISOString())
 
   return (
-    <section className="flex min-h-svh flex-col items-center px-5 pt-24">
-      <h1 className="text-4xl font-bold text-gray-800">Date-Time Dojo</h1>
-      <p className="mt-4 w-full max-w-[50rem] text-center text-lg text-gray-600">
-        A solution for converting dates into different formats. Enter a date and choose from a
-        variety of formats below.
-      </p>
+    <section className="flex min-h-svh flex-col pt-24">
+      <main className="flex flex-col items-center px-5">
+        <h1 className="text-center text-4xl font-bold text-gray-800">Date-Time Dojo</h1>
+        <p className="mt-4 w-full max-w-[50rem] text-center text-lg text-gray-600">
+          Transform dates with ease. Enter a date and choose from a variety of formats below.
+        </p>
 
-      <div className="mt-10 w-full max-w-[40rem] rounded-xl border border-desert-200 bg-white p-4 sm:shadow-[rgba(0,0,0,0.04)_0px_8px_8px]">
-        <DateField
-          granularity="minute"
-          aria-label="Date Input"
-          className="flex rounded-lg border p-2"
-        >
-          <DateInput className="flex flex-1">
-            {segment => (
-              <DateSegment
-                className={({ isPlaceholder }) =>
-                  isPlaceholder ? 'text-gray-400' : 'text-gray-600'
-                }
-                segment={segment}
-              />
-            )}
-          </DateInput>
-          <CalendarDays className="h-6 w-6" />
-        </DateField>
+        <div className="mb-40 mt-10 w-full max-w-[40rem] rounded-xl border border-desert-200 bg-white p-4 sm:shadow-[rgba(0,0,0,0.04)_0px_8px_8px]">
+          <DateField
+            granularity="minute"
+            aria-label="Date Input"
+            className="flex items-center rounded-lg border p-2"
+          >
+            <DateInput className="flex flex-1">
+              {segment => (
+                <DateSegment
+                  className={({ isPlaceholder }) =>
+                    `relative type-literal:px-2 focus:outline-none ${
+                      isPlaceholder ? 'text-gray-400' : 'text-gray-600'
+                    }`
+                  }
+                  segment={segment}
+                />
+              )}
+            </DateInput>
+            <CalendarDays color="#4a4a42" size={20} strokeWidth={1.5} />
+          </DateField>
 
-        <ClientOnly>
-          <div className="mt-10 border-t border-gray-100 pt-2">
-            <div className="flex flex-col gap-6 text-sm">
-              <DateDisplay dateFormat="Locale Date String" formattedDate={ISODate.toString()} />
-              <DateDisplay dateFormat="Timestamp" formattedDate={time} />
-              <DateDisplay dateFormat="Unix Timestamp" formattedDate={unixTime} />
-              <DateDisplay dateFormat="Date String" formattedDate={ISODate.toLocaleString()} />
-              <DateDisplay dateFormat="Date (UTC)" formattedDate={ISODate.toUTCString()} />
+          <React.Suspense key={isMounted ? 'client' : 'server'}>
+            <div className="border-gray-100 pt-2">
+              <div className="flex flex-col gap-6 text-sm">
+                <DateDisplay dateFormat="Date String" formattedDate={ISODate.toLocaleString()} />
+                <DateDisplay dateFormat="Locale Date String" formattedDate={ISODate.toString()} />
+                <DateDisplay dateFormat="Timestamp" formattedDate={time} />
+                <DateDisplay dateFormat="Unix Timestamp" formattedDate={unixTime} />
+                <DateDisplay dateFormat="Date (UTC)" formattedDate={ISODate.toUTCString()} />
 
-              <DateDisplay dateFormat="Date (ISO 8601)" formattedDate={formatISO(ISODate)} />
-              <DateDisplay dateFormat="Date (ISO 9075)" formattedDate={formatISO9075(ISODate)} />
-              <DateDisplay dateFormat="Date (RFC 3339)" formattedDate={formatRFC3339(ISODate)} />
-              <DateDisplay dateFormat="Date (RFC 7231)" formattedDate={formatRFC7231(ISODate)} />
+                <DateDisplay dateFormat="Date (ISO 8601)" formattedDate={formatISO(ISODate)} />
+                <DateDisplay dateFormat="Date (ISO 9075)" formattedDate={formatISO9075(ISODate)} />
+                <DateDisplay dateFormat="Date (RFC 3339)" formattedDate={formatRFC3339(ISODate)} />
+                <DateDisplay dateFormat="Date (RFC 7231)" formattedDate={formatRFC7231(ISODate)} />
 
-              <DateDisplay
-                dateFormat="Excel Format"
-                formattedDate={convertDateToExcelFormat(ISODate)}
-              />
+                <DateDisplay
+                  dateFormat="Excel Format"
+                  formattedDate={convertDateToExcelFormat(ISODate)}
+                />
+              </div>
             </div>
-          </div>
-        </ClientOnly>
-      </div>
+          </React.Suspense>
+        </div>
+      </main>
 
-      <footer className="mt-auto w-full py-4">
-        <div className="mx-auto flex max-w-[50rem] flex-col items-center justify-between gap-2 text-sm text-gray-500 sm:flex-row">
-          <p className="uppercase">All rights reserved &#169; {new Date().getFullYear()}</p>
+      <footer className="mt-auto w-full border-t border-t-desert-200 bg-desert-100 px-5 py-4">
+        <div className="mx-auto flex max-w-[50rem] flex-col items-center justify-center gap-2 text-sm text-gray-500 sm:flex-row">
+          <p className="uppercase"> &#169; {new Date().getFullYear()}</p>
           <p className="hidden uppercase sm:block">
-            <a href="https://github.com/ekrresa" target="_blank" rel="noreferrer noopener">
+            <a href="https://ekrresa.com" target="_blank" rel="noreferrer noopener">
               Ochuko Ekrresa
             </a>
           </p>
