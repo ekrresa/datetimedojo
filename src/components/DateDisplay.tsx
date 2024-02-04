8af132a45f9c6a5444c31cd43e7210ea7b7f3a98
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2024-02-04 22:23:50 +0100

    Improve responsiveness

--- src/components/DateDisplay.tsx
+++ src/components/DateDisplay.tsx
@@ -10,11 +10,12 @@ export function DateDisplay(props: Props) {
   const { dateFormat, formattedDate } = props
 
   return (
-    <div className="flex flex-wrap items-center justify-between gap-1 sm:flex-nowrap sm:gap-4">
-      <p className="shrink-0 grow-0 basis-[125px] font-semibold text-gray-700 sm:text-right">
+    <div className="flex flex-col justify-between sm:flex-row sm:items-center sm:gap-4">
+      <p className="shrink-0 grow-0 font-semibold text-gray-700 sm:basis-[125px] sm:text-right">
         {dateFormat}
       </p>
-      <div className="flex min-w-80 flex-1 items-center justify-between gap-4">
+
+      <div className="flex flex-1 items-center justify-between gap-4 sm:min-w-0">
         <span className="min-w-0 truncate font-mono tabular-nums text-gray-600">
           {formattedDate}
         </span>
